81456140ac448f14a34c5609bf8c253dd802adc2
MuddyTundra23 <guihashish@gmail.com>
2024-06-17 19:00:37 -0300

    input

---
+++ input.py
@@ -0,0 +1,9 @@
+nome = input("Digite o seu nome:")
+idade = int(input("Digite a sua idade:"))
+idade_formatada = formatada = int(idade)
+print(type(idade))
+idade_futura = idade_formatada + 3
+
+
+
+print(f"O nome da pessoa é {nome}, e a idade é {idade_formatada}")
