e6893f63216c6dee36fd9d80a783bae4aeeb7d6d
MuddyTundra23 <guihashish@gmail.com>
2024-06-17 20:10:05 -0300

    desafio_media

---
+++ AULA2/desafio_media.py
@@ -0,0 +1,10 @@
+nome = input("Digite o seu nome:")
+
+n1 = int(input("Digite a sua nota: "))
+n2 = int(input("Digite a sua nota: "))
+n3 = int(input("Digite a sua nota: "))
+n4 = int(input("Digite a sua nota: "))
+
+media = (n1 + n2 + n3 + n4) /4
+
+print(f"Olá, {nome}! Sua média é: {media}")
