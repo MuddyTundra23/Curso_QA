cc3632ca06f31208ea29df36068576e66a98c46c
MuddyTundra23 <guihashish@gmail.com>
2024-06-17 21:09:18 -0300

    Valores

---
+++ AULA2/atividade.py
@@ -0,0 +1,13 @@
+valor = int(input("Qual é o seu numero?"))
+
+result1 = valor * 2
+result2 = valor * 3
+result3 = valor ** 2
+result4 = valor ** (1/2)
+result5 = valor ** (1/3)
+
+print (f"O dobro de {valor} é {result1}")
+print (f"O triplo de {valor} é {result2}")
+print (f"O quadrado de {valor} é {result3}")
+print (f"O raiz quadrada de {valor} é {result4}")
+print (f"O raiz cubica de {valor} é {result4}")
