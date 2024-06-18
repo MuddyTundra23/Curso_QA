502cf9f7049d4507ee6d7fe90c75912f2c0cda08
MuddyTundra23 <guihashish@gmail.com>
2024-06-17 21:39:00 -0300

    lista_2

--- AULA2/lista.py
+++ AULA2/lista.py
@@ -1,6 +1,13 @@
+#Lista
 frutas = [ 'Manga', 'Maracujá', 'Uva']
 
 print(frutas)
 print(frutas[0])
 print(frutas[1])
 print(frutas[2])
+
+#Tupla
+comidas = ('Arroz', 'feijão', 'Batata')
+
+#Set
+facilitadores = {'Vini', 'Matheus', 'Michelle'}
